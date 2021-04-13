ecd083378f8b1f931edf70faea76da41ba42de78
neha6351 <nk6351@g.rit.edu>
2021-04-13 12:04:14 -0400

    Fixed issue 6: Unused parameters, performance issue

--- jadx-plugins/jadx-dex-input/src/main/java/jadx/plugins/input/dex/sections/DexHeader.java
+++ jadx-plugins/jadx-dex-input/src/main/java/jadx/plugins/input/dex/sections/DexHeader.java
@@ -31,12 +31,8 @@ public class DexHeader {
 		if (endianTag != DexConsts.ENDIAN_CONSTANT) {
 			throw new DexException("Unexpected endian tag: 0x" + Integer.toHexString(endianTag));
 		}
-		//TODO : Remove unused variable
-		//FIXME : linkSize, linkOff, stringIdsSize was not used, hence can be deleted
-		int linkSize = buf.readInt();
-		int linkOff = buf.readInt();
+
 		int mapListOff = buf.readInt();
-		int stringIdsSize = buf.readInt();
 		stringIdsOff = buf.readInt();
 		typeIdsSize = buf.readInt();
 		typeIdsOff = buf.readInt();
